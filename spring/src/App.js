5e151fc4fdcccc641414d80d4f153725ed25b928
everglow <78077569+okchanho@users.noreply.github.com>
2021-12-05 16:33:38 +0900

    useEffect

--- spring/src/App.js
+++ spring/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import {num} from './Sub';
 import Sub from './Sub';
 /*
 0. React엔진 - 데이터변경 감지에서 UI를 그려주는 엔진
@@ -11,50 +12,16 @@ import Sub from './Sub';
 (3) if사용 불가능 => 삼항연산자 사용가능 (조건 ? 값(true) : 값(false))
 (4) 조건부 렌더링(조건 && 값 true)
 (5) css 디자인 
-      - 내부에 적는방법
-      - 외부 파일에 적는 방법
-      - 라이브러리 사용(부트스트랩, component-styled)
-  (6)부모 컴포넌트가 변경되면 자식 컴포넌트에 데이터가 변화되는지 확인한번 해본다.
+- 내부에 적는방법
+- 외부 파일에 적는 방법
+- 라이브러리 사용(부트스트랩, component-styled)
+(6)부모 컴포넌트가 변경되면 자식 컴포넌트에 데이터가 변화되는지 확인한번 해본다.
 */
 function App() {
-  //다운로드 받음
-  console.log('App이 실행됨')
-  const [users, setusers] = useState([
-    {id:1, name:"홍길동"},
-    { id:2, name:"임꺽정"},
-    { id:3, name:"장보고"},
-    { id:4, name:"월터"}]
-    );
-
-  const download = ()=>{
-    let sample = [      
-      {id:1, name:"홍길동"},
-      { id:2, name:"임꺽정"},
-      { id:3, name:"장보고"},
-      { id:4, name:"월터"}
-    ];
-    setusers(sample); //데이터가 같지만 레퍼런스가 다르다
-  }
-
-  // const [number, setnumber] = useState(1); //React안에 hooks 라이브러리 상태값이 된다.  
-  // //let number = 1; 상태값이 아님
-
-  // const add = ()=>{
-  //   setnumber(number+1);//리엑트한테 number값 변경할게 요청    
-  //   console.log("add", number);    
-  // }
-  //랜더링 시점 = 상태값이 변경 
-  //상태가 변경되면 전체가 Rebulid가 된다. 
-
 
   return (
-    <div>      
-        <button onClick={download}>다운로드</button> 
-        {users.map((u)=>(
-          <h1>
-            {u.id}, {u.name}
-          </h1>
-        ))}
+    <div>
+        <h1>{num}</h1>              
         <Sub/>
     </div>
   );
--- spring/src/Sub.js
+++ spring/src/Sub.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import {React, useEffect,useState} from 'react'
 
-const Sub = () => {
+let num = 10;
 
+
+function Sub() {
+    const [data, setdata] = useState(0);
+
+    const download=() =>{
+        //다운로드 받고 (통신)
+        let downloadData = 5; //가정
+        setdata()
+    }
+
+    useEffect(() => {
+        console.log('App useEffect 실행됨')
+        setdata(5);
+    },[])
+
+
+
+    //실행시점 
+    /*
+    (1) App()함수가 최초 한번 실행될때 실행된다.(마운트될때) 그림이 그려질때 최초로 
+    (2) 상태변수가 변경될때
+    */
     return (
         <div>
-           <h1>sub입니다.</h1>
+            <h1>데이터 : {data}</h1>
+            <button onClick={()=>{
+                setdata(data+1);
+            }}>더하기</button>
+            서브임돠
         </div>
-    );
-};
-
-export default Sub;
+    )
+}
+export {num};
+export default Sub
